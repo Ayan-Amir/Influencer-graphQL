29f0083851d3e655274b026e3dc9e81a0370dd6b
Kashif Arif <kashif@kuwaitnet.com>
2021-11-18 13:04:23 +0500

    login page updates

--- src/router/index.js
+++ src/router/index.js
@@ -5,66 +5,62 @@ import DashboardLayout from '@/layouts/DashboardLayout.vue';
 import user from '@/views/user/router';
 Vue.use(VueRouter);
 
-const routes = [
-	{
-		path: "/login",
-		component: () =>
-        import ("@/views/common/Login.vue")
-	},
-	{
-		path: "/user",
-		component: DashboardLayout,
-		children:[...user]
-	},
-	{
-		path: "/user/login",
-		component: LoginOrRegister,
-		children:[
-			{
-				path: "",
-				component: () =>
-        		import ("@/views/user/Login.vue"),
-			}
-		]
-	},
-	{
-		path: "/user/register",
-		component: LoginOrRegister,
-		children:[
-			{
-				path: "",
-				component: () =>
-        		import ("@/views/user/register/Register.vue"),
-			},
-			{
-				path: "welcome",
-				component: () =>
-        		import ("@/views/user/register/Welcome.vue"),
-			},
-			{
-				path: "profile",
-				component: () =>
-        		import ("@/views/user/register/Profile.vue"),
-			},
-			{
-				path: "profile-photo",
-				component: () =>
-        		import ("@/views/user/register/ProfilePhoto.vue"),
-			},
-			{
-				path: "payment-details",
-				component: () =>
-        		import ("@/views/user/register/PaymentDetails.vue"),
-			}
-		]
-	},
-  	
+const routes = [{
+        path: "/login",
+        component: () =>
+            import ("@/views/common/Login.vue")
+    },
+    {
+        path: "/user",
+        component: DashboardLayout,
+        children: [...user]
+    },
+    {
+        path: "/user/login",
+        component: LoginOrRegister,
+        children: [{
+            path: "",
+            component: () =>
+                import ("@/views/user/Login.vue"),
+        }]
+    },
+    {
+        path: "/user/register",
+        component: LoginOrRegister,
+        children: [{
+                path: "",
+                component: () =>
+                    import ("@/views/user/register/Register.vue"),
+            },
+            {
+                path: "welcome",
+                component: () =>
+                    import ("@/views/user/register/Welcome.vue"),
+            },
+            {
+                path: "profile",
+                component: () =>
+                    import ("@/views/user/register/Profile.vue"),
+            },
+            {
+                path: "profile-photo",
+                component: () =>
+                    import ("@/views/user/register/ProfilePhoto.vue"),
+            },
+            {
+                path: "payment-details",
+                component: () =>
+                    import ("@/views/user/register/PaymentDetails.vue"),
+            }
+        ]
+    },
+
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
 });
 
 export default router;
