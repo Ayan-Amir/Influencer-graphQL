81aa4cffbbc3c5b08b3cc8e772f8ed8ef6cb7bb0
logicsdrive <info@logicsdrive.com>
2021-11-24 19:44:48 +0500

    updates

--- src/views/user/FreeOffers.vue
+++ src/views/user/FreeOffers.vue
@@ -4,18 +4,17 @@
 			@locationvalue="locationValue"
 			@categoryvalue="categoryValue"
 		/>
+		<div v-if="$apollo.loading">Loading...</div>
 		<div class="row">
 			<div
 				class="col-xl-3 col-lg-4 col-sm-6"
-				v-for="cardData in offers"
-				:key="cardData.id"
+				v-for="offer in offers"
+				:key="offer.id"
 			>
-				<offers-card
-					:cardData="cardData"
-					v-for="offerCard in offerCard"
-					:key="offerCard.index"
-					:offerCard="offerCard"
-				/>
+				<!-- <offers-card
+					:cardData="offer"
+					:offerCard="offer"
+				/> -->
 			</div>
 		</div>
 	</div>
@@ -29,227 +28,20 @@ export default {
 			offers: [],
 			locations: '',
 			category: '',
-			offerCard: [
-				{
-					index: 0,
-					mainImage: 'offers-img1.png',
-					time: '33 : 16 : 02',
-					title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-					brandLogo: 'logo1.png',
-					imageLists: [
-						{
-							id: 1,
-							img: 'user-img.jpg',
-						},
-						{
-							id: 2,
-							img: '',
-						},
-						{
-							id: 3,
-							img: 'user-img.jpg',
-						},
-						{
-							id: 4,
-							img: '',
-						},
-					],
-					number: '+152',
-				},
-				{
-					index: 1,
-					mainImage: 'offers-img1.png',
-					time: '33 : 16 : 02',
-					title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-					brandLogo: 'logo1.png',
-					imageLists: [
-						{
-							id: 1,
-							img: 'user-img.jpg',
-						},
-						{
-							id: 2,
-							img: '',
-						},
-						{
-							id: 3,
-							img: 'user-img.jpg',
-						},
-						{
-							id: 4,
-							img: '',
-						},
-					],
-					number: '+152',
-				},
-				{
-					index: 2,
-					mainImage: 'offers-img1.png',
-					time: '33 : 16 : 02',
-					title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-					brandLogo: 'logo1.png',
-					imageLists: [
-						{
-							id: 1,
-							img: '',
-						},
-						{
-							id: 2,
-							img: '',
-						},
-						{
-							id: 3,
-							img: '',
-						},
-						{
-							id: 4,
-							img: '',
-						},
-					],
-					number: '+152',
-				},
-				{
-					index: 3,
-					mainImage: 'offers-img1.png',
-					time: '33 : 16 : 02',
-					title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-					brandLogo: 'logo1.png',
-					imageLists: [
-						{
-							id: 1,
-							img: '',
-						},
-						{
-							id: 2,
-							img: '',
-						},
-						{
-							id: 3,
-							img: '',
-						},
-						{
-							id: 4,
-							img: '',
-						},
-					],
-					number: '+152',
-				},
-				{
-					index: 4,
-					mainImage: 'offers-img1.png',
-					time: '33 : 16 : 02',
-					title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-					brandLogo: 'logo1.png',
-					imageLists: [
-						{
-							id: 1,
-							img: '',
-						},
-						{
-							id: 2,
-							img: '',
-						},
-						{
-							id: 3,
-							img: '',
-						},
-						{
-							id: 4,
-							img: '',
-						},
-					],
-					number: '+152',
-				},
-				{
-					index: 5,
-					mainImage: 'offers-img1.png',
-					time: '33 : 16 : 02',
-					title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-					brandLogo: 'logo1.png',
-					imageLists: [
-						{
-							id: 1,
-							img: '',
-						},
-						{
-							id: 2,
-							img: '',
-						},
-						{
-							id: 3,
-							img: '',
-						},
-						{
-							id: 4,
-							img: '',
-						},
-					],
-					number: '+152',
-				},
-				{
-					index: 6,
-					mainImage: 'offers-img1.png',
-					time: '33 : 16 : 02',
-					title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-					brandLogo: 'logo1.png',
-					imageLists: [
-						{
-							id: 1,
-							img: '',
-						},
-						{
-							id: 2,
-							img: '',
-						},
-						{
-							id: 3,
-							img: '',
-						},
-						{
-							id: 4,
-							img: '',
-						},
-					],
-					number: '+152',
-				},
-				{
-					index: 7,
-					mainImage: 'offers-img1.png',
-					time: '33 : 16 : 02',
-					title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-					brandLogo: 'logo1.png',
-					imageLists: [
-						{
-							id: 1,
-							img: '',
-						},
-						{
-							id: 2,
-							img: '',
-						},
-						{
-							id: 3,
-							img: '',
-						},
-						{
-							id: 4,
-							img: '',
-						},
-					],
-					number: '+152',
-				},
-			],
+			page: 1
 		};
 	},
 	apollo: {
 		offers: {
 			query: require('../../graphql/offers.gql'),
-			variables: {
-				// locations,
+			variables() {
+				return{
+					page: this.page
+				}
 			},
-			update(data) {
-				return data.offers;
-			},
+			// update(data) {
+			// 	console.log(data);
+			// },
 		},
 	},
 	methods: {
@@ -261,10 +53,14 @@ export default {
 			this.category = e;
 			console.log(this.category);
 		},
+		
 	},
 	components: {
 		OffersCard,
 	},
+	mounted(){
+		console.log(IMG_BASE);
+	}
 };
 </script>
 
