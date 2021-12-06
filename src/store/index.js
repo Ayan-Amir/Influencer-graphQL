5f106c4987bbee49aa5e28363145bbb6c0b9f67f
logicsdrive <info@logicsdrive.com>
2021-12-06 20:15:24 +0500

    store fix

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import {alert} from './alert'
 import auth from './auth'
-import register from './register'
 
 Vue.use(Vuex)
 const debug = process.env.NODE_ENV !== 'production'
