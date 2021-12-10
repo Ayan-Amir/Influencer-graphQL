105f886eac977b31bf457c6296ab4b0cb8f74c8d
Kashif Arif <kashif@kuwaitnet.com>
2021-12-10 18:42:44 +0500

    modal updates

--- src/views/brand/InfluencerDetail.vue
+++ src/views/brand/InfluencerDetail.vue
@@ -1,94 +1,227 @@
 <template>
-	<div class="container">
-		<div class="influencerDetail">
-			<div class="influencerDetail__left">
-				<div class="influencerDetail__left--image onDesktop">
-					<img
-						src="@/assets/images/iPhone-12.png"
-						alt=""
-						class="img-fluid"
-					/>
-				</div>
-				<!-- hide on Desktop -->
-
-				<div class="onMobile">
-					<ul>
-						<li class="influencerDetail__left--icon">
-							<img
-								src="@/assets/images/iPhone-12.png"
-								alt=""
-								class="img-fluid"
-							/>
-						</li>
-						<li>
-							<h1>Dorian Hâtz Popa</h1>
-							<span class="influencerDetail__right--subTitle"
-								>Bucharest</span
-							>
-						</li>
-					</ul>
-				</div>
-			</div>
-			<div class="influencerDetail__right">
-				<!-- hide on Mobile -->
-				<div class="onDesktop">
-					<h1>Dorian Hâtz Popa</h1>
-					<span class="influencerDetail__right--subTitle"
-						>Bucharest</span
-					>
-				</div>
-				<div class="row">
-					<div
-						class="col-6 col-xl-3"
-						v-for="item in socialMedia"
-						:key="item.id"
-					>
-						<social-media :socialMedia="item" />
-					</div>
-				</div>
-				<ul class="influencerDetail__right--hashtags">
-					<li><a href="#">#newpost.</a></li>
-					<li><a href="#">#influencer.</a></li>
-					<li><a href="#">#blogger.</a></li>
-					<li><a href="#">#digitalmarketing.</a></li>
-					<li><a href="#">#fashionblogger.</a></li>
-				</ul>
-				<p>
-					Dorian Hâtz Popa August 7, 1988, Constanța, Romanian
-					vlogger, composer, dancer and singer. He became known in
-					2011, with his appearance in the series Bet with life with
-					his colleagues from Lala Band, where he played Andrei
-					Anghel, a series that received the award for “best debut” at
-					the VIP Awards Gala.
-				</p>
-				<div class="influencerDetail__right--btnWrapper">
-					<h2>Price: 1.000 RON</h2>
-					<div class="inline-btns onDesktop">
-						<a href="#" class="btn btn-primary">Buy</a>
-						<a href="#" class="btn btn-primary">Message</a>
-					</div>
-					<div class="inline-btns onMobile">
-						<a href="#" class="btn btn-primary">Buy Offer</a>
-					</div>
-				</div>
-				<div class="d-flex align-items-center mt-3">
-					<div class="influencerDetail__right--icon">
+	<div>
+		<div class="container">
+			<div class="influencerDetail">
+				<div class="influencerDetail__left">
+					<div class="influencerDetail__left--image onDesktop">
 						<img
-							src="@/assets/images/icons/avatar.png"
+							src="@/assets/images/iPhone-12.png"
 							alt=""
 							class="img-fluid"
 						/>
 					</div>
-					<span>Sep 10, 2021</span>
+					<!-- hide on Desktop -->
+
+					<div class="onMobile">
+						<ul>
+							<li class="influencerDetail__left--icon">
+								<img
+									src="@/assets/images/iPhone-12.png"
+									alt=""
+									class="img-fluid"
+								/>
+							</li>
+							<li>
+								<h1>Dorian Hâtz Popa</h1>
+								<span class="influencerDetail__right--subTitle"
+									>Bucharest</span
+								>
+							</li>
+						</ul>
+					</div>
 				</div>
-				<div class="row mt-3">
-					<div class="col-md-4">
-						<influencer-detail-gallery />
+				<div class="influencerDetail__right">
+					<!-- hide on Mobile -->
+					<div class="onDesktop">
+						<h1>Dorian Hâtz Popa</h1>
+						<span class="influencerDetail__right--subTitle"
+							>Bucharest</span
+						>
+					</div>
+					<div class="row">
+						<div
+							class="col-6 col-xl-3"
+							v-for="item in socialMedia"
+							:key="item.id"
+						>
+							<social-media :socialMedia="item" />
+						</div>
+					</div>
+					<ul class="influencerDetail__right--hashtags">
+						<li><a href="#">#newpost.</a></li>
+						<li><a href="#">#influencer.</a></li>
+						<li><a href="#">#blogger.</a></li>
+						<li><a href="#">#digitalmarketing.</a></li>
+						<li><a href="#">#fashionblogger.</a></li>
+					</ul>
+					<p>
+						Dorian Hâtz Popa August 7, 1988, Constanța, Romanian
+						vlogger, composer, dancer and singer. He became known in
+						2011, with his appearance in the series Bet with life with
+						his colleagues from Lala Band, where he played Andrei
+						Anghel, a series that received the award for “best debut” at
+						the VIP Awards Gala.
+					</p>
+					<div class="influencerDetail__right--btnWrapper">
+						<h2>Price: 1.000 RON</h2>
+						<div class="inline-btns onDesktop">
+							<a href="#" class="btn btn-primary" v-b-modal.introduction>Buy</a>
+							<a href="#" class="btn btn-primary">Message</a>
+						</div>
+						<div class="inline-btns onMobile">
+							<a href="#" class="btn btn-primary">Buy Offer</a>
+						</div>
+					</div>
+					<div class="d-flex align-items-center mt-3">
+						<div class="influencerDetail__right--icon">
+							<img
+								src="@/assets/images/icons/avatar.png"
+								alt=""
+								class="img-fluid"
+							/>
+						</div>
+						<span>Sep 10, 2021</span>
+					</div>
+					<div class="row mt-3">
+						<div class="col-md-4">
+							<influencer-detail-gallery />
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
+		<!-- Introduction modal -->
+		<b-modal ref="introduction" id="introduction" centered>
+			<div class="title">Instruction</div>
+			<p class="subTitle">
+				What do you want the local Influencers to include in their
+				posts?
+			</p>
+			<form>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="@Our lnstagram">
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="#Our hashtag">
+				</div>
+				<button type="button" class="btn btn-default full">
+					Add images or videos
+				</button>
+				<div class="form-group">
+					<label class="control-label"
+						>Instructions and campaign details for influencers:
+					</label>
+					<textarea
+						class="form-control"
+						placeholder="Ex: Iwant you to talk about FameUp"
+					></textarea>
+				</div>
+				<button type="button" class="btn btn-primary" @click="hideModal" v-b-modal.paymentMethod>continue</button>
+			</form>
+		</b-modal>
+		<!-- modal End-->
+
+		<!-- Payment Method modal -->
+		<b-modal ref="paymentMethod" id="paymentMethod" centered>
+			<div class="title">Payment Method</div>
+			<p class="subTitle">
+				Card Details
+			</p>
+			<form>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Card Number">
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Expiry Date">
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="CVV">
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group mt-0">
+							<input type="text" class="form-control" placeholder="First name">
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group mt-0">
+							<input type="text" class="form-control" placeholder="Last name">
+						</div>
+					</div>
+				</div>
+				<button type="button" class="btn btn-primary" v-b-modal.invoicingDetail @click="hideModal2">Conﬁrm Payment</button>
+			</form>
+		</b-modal>
+		<!-- modal End-->
+
+		<!-- Invocing Detail modal -->
+		<b-modal  ref="invoicingDetail" id="invoicingDetail" centered>
+			<div class="title">Add invoicing details</div>
+			<ul class="checkbox list-unstyled">
+				<li>
+					<label>
+						<input
+							type="checkbox"
+							name="Individual"
+						/>
+						<span
+							>Individual?</span
+						>
+					</label>
+				</li>
+			</ul>
+			<form>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Company name">
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="VAT number ">
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Trade registration number">
+				</div>
+				<div class="form-group">
+					<input type="email" class="form-control" placeholder="Email ">
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group mt-0">
+							<input type="text" class="form-control" placeholder="Country">
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group mt-0">
+							<input type="text" class="form-control" placeholder="Locality">
+						</div>
+					</div>
+				</div>
+				<div class="form-group mt-0">
+					<input type="text" class="form-control" placeholder="Address  ">
+				</div>
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Post code  ">
+				</div>
+				<p class="subTitle legal">
+				Legal representative of the company
+			</p>
+			<div class="row">
+					<div class="col-md-6">
+						<div class="form-group mt-0">
+							<input type="text" class="form-control" placeholder="First name">
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="form-group mt-0">
+							<input type="text" class="form-control" placeholder="Last name">
+						</div>
+					</div>
+				</div>
+				<button type="button" class="btn btn-primary" @click="hideModal3">Save</button>
+			</form>
+		</b-modal>
+		<!-- modal End-->
 	</div>
+	
 </template>
 
 <script>
@@ -97,6 +230,19 @@ import InfluencerDetailGallery from '@/components/brand/InfluencerDetailGallery.
 
 export default {
 	components: { SocialMedia, InfluencerDetailGallery },
+	methods: {
+
+		hideModal() {
+			this.$refs['introduction'].hide()
+		},
+		hideModal2() {
+			this.$refs['paymentMethod'].hide()
+		},
+		hideModal3() {
+			this.$refs['invoicingDetail'].hide()
+		},
+		
+	},
 	data() {
 		return {
 			socialMedia: [
