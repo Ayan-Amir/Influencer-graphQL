c08d282d01fcdcb3a43ab90e9f559e5aed90287e
Kashif Arif <kashif@kuwaitnet.com>
2021-12-16 11:44:53 +0500

    updates

--- src/views/brand/router/register.js
+++ src/views/brand/router/register.js
@@ -3,38 +3,10 @@ export default [{
         component: () =>
             import ("@/views/brand/register/Register.vue"),
     },
-    {
-        path: "welcome",
-        component: () =>
-            import ("@/views/brand/register/Welcome.vue"),
-    },
+
     {
         path: "number-registration",
         component: () =>
             import ("@/views/brand/register/NumberRegistration.vue"),
-    },
-    {
-        path: "profile-photo",
-        component: () =>
-            import ("@/views/brand/register/ProfilePhoto.vue"),
-    },
-    {
-        path: "connect-social",
-        component: () =>
-            import ("@/views/brand/register/ConnectSocial.vue"),
-    },
-    // {
-    //   path: "payment-details",
-    //   component: () => import("@/views/user/register/PaymentDetails.vue"),
-    // },
-    {
-        path: "password",
-        component: () =>
-            import ("@/views/brand/register/Password.vue"),
-    },
-    {
-        path: "story-price",
-        component: () =>
-            import ("@/views/brand/register/StoryPrice.vue"),
     },
 ];
