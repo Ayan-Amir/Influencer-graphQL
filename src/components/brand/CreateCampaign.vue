d599d40b21d071c93f77ff0286a4ba92fa1fc52f
atif <info@logicsdrive.com>
2022-01-07 17:49:08 +0500

    update

--- src/components/brand/CreateCampaign.vue
+++ src/components/brand/CreateCampaign.vue
@@ -474,6 +474,18 @@ export default {
 				transform: translateY(-50%);
 				z-index: 1;
 			}
+			&::after {
+				content: '';
+				position: absolute;
+				width: 9px;
+				height: 14px;
+				background-image: url("data:image/svg+xml,%0A%3Csvg xmlns='http://www.w3.org/2000/svg' width='7.765' height='13.441' viewBox='0 0 7.765 13.441'%3E%3Cpath id='Path_210' data-name='Path 210' d='M4871.166,592.811l5.351,5.225-5.351,5.388' transform='translate(-4869.752 -591.396)' fill='none' stroke='%239637f1' stroke-linecap='round' stroke-linejoin='round' stroke-width='2'/%3E%3C/svg%3E%0A");
+				background-repeat: no-repeat;
+				right: 19px;
+				top: 50%;
+				transform: translateY(-50%) rotate(90deg);
+				position: absolute;
+			}
 			.form-control {
 				padding-left: rem(32px);
 			}
